d323e05f93712430c3789cdf7106cbe45e6a6788
Soham Patil <patilsoham390@gmail.com>
2024-07-17 16:39:18 +0530

    solved few problems

--- leet.py
+++ leet.py
@@ -373,20 +373,17 @@
 # print(soham(nums))
 
 
-nums = [0,0,1,1,1,2,2,3,3,4]
-def soham(nums):
-    if not nums:
-        return 0
-
-    write_index = 1
-
-    for i in range(1, len(nums)):
-        if nums[i] != nums[i - 1]:
-            print(i, nums)
-            nums[write_index] = nums[i]
-            print(i, nums)
-            write_index += 1
-
-    return write_index
-
-print(soham(nums))
+board = [["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+def soham(board):
+    for i in range(0, len(board)):
+        print(board[i])
+    return True
+print(soham(board))
