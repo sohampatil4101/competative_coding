6026dc54fec4776bc508dc6372f8ddd66f0ffd0b
Soham Patil <patilsoham390@gmail.com>
2024-07-21 23:23:41 +0530

    solved few problems

--- pattern.py
+++ pattern.py
@@ -19,19 +19,19 @@
 # Hollow_Square_Star_Pattern()
 
 
-# def Right_Angle_Triangle_Pattern():
-#     n = int(input("Enter Number of rows"))
-#     for i in range(0, n):
-#         print(" " * (n-(i+1)), end="")
-#         print("*" * (i+1))
-# Right_Angle_Triangle_Pattern()
+def Right_Angle_Triangle_Pattern():
+    n = 5
+    for i in range(0, n):
+        print(" " * (n-(i+1)), end="")
+        print("*" * (i+1))
+Right_Angle_Triangle_Pattern()
 
 
-# def Right_Angle_Triangle_Pattern():
-#     n = int(input("Enter Number of rows"))
-#     for i in range(0, n):
-#         print("*" * (i+1))
-# Right_Angle_Triangle_Pattern()
+def Right_Angle_Triangle_Pattern():
+    n = 5
+    for i in range(0, n):
+        print("*" * (i+1))
+Right_Angle_Triangle_Pattern()
 
 
 # def Pyramid_Star_Pattern():
