b49a032286741c712ca5fa7824cc2933161d0e32
Soham Patil <patilsoham390@gmail.com>
2024-07-21 22:27:47 +0530

    solved few problems

--- pattern.py
+++ pattern.py
@@ -1,19 +1,37 @@
 # https://prepinsta.com/pattern-programs-tutorial/
 
 
-def Square_Star_Pattern():
-    n = int(input("Enter Number of rows"))
+# def Square_Star_Pattern():
+#     n = int(input("Enter Number of rows"))
 
-    for i in range(0, n):
-        print("*"*n)
+#     for i in range(0, n):
+#         print("*"*n)
 
-Square_Star_Pattern()
+# Square_Star_Pattern()
 
 
-def Hollow_Square_Star_Pattern():
-    n = int(input("Enter Number of rows"))
+# def Hollow_Square_Star_Pattern():
+#     n = int(input("Enter Number of rows"))
 
-    for i in range(0, n):
-        print("*"*n)
+#     for i in range(0, n):
+#         print("*"*n)
 
-Hollow_Square_Star_Pattern()
+# Hollow_Square_Star_Pattern()
+
+
+# def Right_Angle_Triangle_Pattern():
+#     n = int(input("Enter Number of rows"))
+#     for i in range(0, n):
+#         print(" " * (n-(i+1)), end="")
+#         print("*" * (i+1))
+# Right_Angle_Triangle_Pattern()
+
+
+# def Right_Angle_Triangle_Pattern():
+#     n = int(input("Enter Number of rows"))
+#     for i in range(0, n):
+#         print("*" * (i+1))
+# Right_Angle_Triangle_Pattern()
+
+
+
