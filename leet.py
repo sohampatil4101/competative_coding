3ee0dc7f762f104ebe3dd4a5a67ff337a81c4b0c
Soham Patil <patilsoham390@gmail.com>
2024-07-31 01:04:59 +0530

    solved few problems

--- leet.py
+++ leet.py
@@ -412,51 +412,51 @@
 
 
 
-class Node:
-    def __init__(self, data=None):
-        self.data = data
-        self.next = None
+# class Node:
+#     def __init__(self, data=None):
+#         self.data = data
+#         self.next = None
 
-class LinkedList:
-    def __init__(self):
-        self.head = None
+# class LinkedList:
+#     def __init__(self):
+#         self.head = None
 
-    def append(self, data):
-        new_node = Node(data)
-        if self.head is None:
-            self.head = new_node
-        else:
-            current = self.head
-            while current.next:
-                current = current.next
-            current.next = new_node
+#     def append(self, data):
+#         new_node = Node(data)
+#         if self.head is None:
+#             self.head = new_node
+#         else:
+#             current = self.head
+#             while current.next:
+#                 current = current.next
+#             current.next = new_node
 
 
-    def display(self):
-        current = self.head
-        while current:
-            print(current.data, end=" -> ")
-            current = current.next
-        print("None")
+#     def display(self):
+#         current = self.head
+#         while current:
+#             print(current.data, end=" -> ")
+#             current = current.next
+#         print("None")
 
-    def display(self):
-        current = self.head
-        if(current):
-            while current:
-                if(current.next is None):
-                    print(current.data)
-                else:
-                    print(current.data, "->", end="")
-                current = current.next
-        else:
-            print("linkedlist is empty")
+#     def display(self):
+#         current = self.head
+#         if(current):
+#             while current:
+#                 if(current.next is None):
+#                     print(current.data)
+#                 else:
+#                     print(current.data, "->", end="")
+#                 current = current.next
+#         else:
+#             print("linkedlist is empty")
 
-# Usage
-linked_list = LinkedList()
-linked_list.append(1)
-linked_list.append(2)
-linked_list.append(3)
-linked_list.display()  # Output: 1 -> 2 -> 3 -> None
+# # Usage
+# linked_list = LinkedList()
+# linked_list.append(1)
+# linked_list.append(2)
+# linked_list.append(3)
+# linked_list.display()  # Output: 1 -> 2 -> 3 -> None
 
 
 
